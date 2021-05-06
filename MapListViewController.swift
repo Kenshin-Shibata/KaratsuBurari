52b5d71794205f8e412efa7ffd33c735360df5d9
Kenshin-Shibata <ps0139hk@gmail.com>
2021-05-07 00:22:38 +0900

    change: #28 MapButtonをMapListへ変更

--- MapListViewController.swift
+++ MapListViewController.swift
@@ -7,23 +7,55 @@
 
 import UIKit
 
-class MapListViewController: UIViewController {
-
+class MapListViewController: UIViewController , UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var MapListTable: UITableView!
+    
+    let maps: [String] = [
+                        "現在の唐津",
+                        "1960年代の唐津",
+                        "1980年代の唐津",
+                        "江戸時代の唐津"
+                        ]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return maps.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = MapListTable.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = maps[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            // セルの選択を解除
+            // 遷移前にセルの選択状態を解除しておかないと、Appleの審査に落ちることがあるらしい
+            tableView.deselectRow(at: indexPath, animated: true)
+            
+            // performSegue(withIdentifier: "toTestView", sender: nil)
+    
+        // 別の画面に遷移
+        switch indexPath.row {
+        case 0:
+            performSegue(withIdentifier: "toFirstMap", sender: nil)
+        case 1:
+            performSegue(withIdentifier: "toSecondMap", sender: nil)
+        case 2:
+            performSegue(withIdentifier: "toThirdMap", sender: nil)
+        default:
+            break
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        MapListTable.dataSource = self
+        MapListTable.delegate = self
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
